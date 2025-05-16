a48f5df62cc8110ed9f003713badac54bf9d268e
DESKTOP-ISS0LQL\user1 <dijae9@gmail.com>
2025-05-16 17:54:52 +0900

    [완료] 주문시작페이지(orderStart.jsp),주문관련스타일페이지(order.css) [진행중] 주문 테스트 페이지 (orderTestForm.html), 주문관련 컨트롤러 페이지 OrderStartServelet.java

--- semi_project/src/main/java/kr/or/iei/order/controller/OrderStartServlet.java
+++ semi_project/src/main/java/kr/or/iei/order/controller/OrderStartServlet.java
@@ -1,11 +1,15 @@
 package kr.or.iei.order.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kr.or.iei.product.model.vo.Product;
 
 /**
  * Servlet implementation class OrderStartServlet
@@ -26,8 +30,41 @@ public class OrderStartServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//1. 인코딩 설정 - 필터에서 진행
+		
+		//2. 값 추출
+		String productId = request.getParameter("productId");
+        String productName = request.getParameter("productName");
+        int productPrice = Integer.parseInt(request.getParameter("productPrice"));
+        String deliveryMethod = request.getParameter("deliveryMethod");        
+		
+        //3. 로직
+        Product product = new Product();
+        product.setProductNo(productId);
+        product.setProductName(productName);
+        product.setProductPrice(productPrice);
+        product.setTradeMethodCode(deliveryMethod);
+        
+        	//3.1 총 결제 예상 금액 계산
+        	int deliveryFee = 0;
+        	if("M1".equals(deliveryMethod)) { //M1 = 선불, M3 = 후불
+        		deliveryFee = 5000; // 일반 택배
+        	}else if ("M3".equals(deliveryMethod))
+        		deliveryFee = 0;
+        	
+        	int totalProductAmount = productPrice + deliveryFee;
+        
+        
+        //3.2 결과값 전송
+    	request.setAttribute("product", product); // 상품 정보        
+        request.setAttribute("deliveryMethod", deliveryMethod); // 선택된 배송 방법        
+        request.setAttribute("deliveryFee", deliveryFee); // 배송비
+        request.setAttribute("totalProductAmount", totalProductAmount); // 총 상품 금액
+        
+    	//4. 결과 처리
+        RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/order/orderStart.jsp");
+        view.forward(request, response);        	
+        
 	}
 
 	/**
